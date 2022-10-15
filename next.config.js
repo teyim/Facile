b8a515a86b73d68bc8c7cc930c03fb1a8660c43a
teyim <teyimasobo@gmail.com>
2022-10-15 17:55:34 +0100

    refactor: repo cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
